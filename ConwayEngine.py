41537aa00f4007f041eebc1e97d39b54d2da1280
martinrs <maros105@gmail.com>
2022-02-08 13:41:41 +0100

    Template for implementing Conway GoL engine

---
+++ ConwayEngine.py
@@ -0,0 +1,32 @@
+class World():
+
+    def __init__(self, width, height):
+        pass
+
+    def flip(self, x, y):
+        pass
+
+    def countLiveNeighbours(self, x, y):
+        pass
+
+    def update(self):
+        pass
+
+    def getDimensions(self):
+        pass
+
+class Grid():
+
+    def __init__(self, width, height):
+        pass
+
+    def update(self, newCells):
+        pass
+
+class Cell:
+
+    def __init__(self, alive=False):
+        pass
+
+    def toggleAlive(self):
+        pass
